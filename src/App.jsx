51156d76588af7443c570c63966695fef600bcb5
Dhaval <joshidhaval2002@gmail.com>
2025-06-29 13:46:20 +0530

    Created appStore and UserSlice and provided store to the main app

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            {/* below are children routes in body we will be using Outlet to based on req redner component */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-     
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              {/* below are children routes in body we will be using Outlet to based on req redner component */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
 
 export default App;
-
