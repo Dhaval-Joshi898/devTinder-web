36a47531d902007691eca05a6362140e070c22a0
Dhaval <joshidhaval2002@gmail.com>
2025-07-01 15:47:34 +0530

    edit profile feature and also preview of showing

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -4,18 +4,19 @@ const UserCard = ({ userData }) => {
   // const feed=useSelector((store)=>store?.feed)
   const { firstName, lastName, age, gender, photoUrl, about, skills } =
     userData;
+    console.log("ABOUT ins card"+about)
+    console.log("ABOUT ins card"+gender)
 
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure className="h-72 bg-base-200 flex items-center justify-center">
         {/* <figure className="h-72 overflow-hidden "> */}
-        <img src={photoUrl}
-         alt="user img"
-        className="h-full  object-contain" />
+        <img src={photoUrl} alt="user img" className="h-full  object-contain" />
       </figure>
 
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
+        
         {age && gender && <p>{age + "," + gender}</p>}
         <p>{about}</p>
         {skills && <p>{"Best in:" + " " + skills.join(", ")}</p>}
