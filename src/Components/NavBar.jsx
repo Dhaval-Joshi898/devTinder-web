880758ce33815f1fa5917f846f7cf0323d8123ff
Dhaval <joshidhaval2002@gmail.com>
2025-08-13 20:54:12 +0530

    made the navbar responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = () => {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const requests = useSelector((store) => store.requests);
 
   const handleLogoutClick = async () => {
     try {
@@ -25,10 +26,10 @@ const NavBar = () => {
 
   return (
     <div className="navbar bg-base-300 shadow-sm fixed top-0 z-[999] ">
-      <div className="flex-1 ml-5">
+      <div className="flex-1 ml-5 ">
         {user ? <Link to="/">🧑🏻‍💻DevTinder</Link> : <a>🧑🏻‍💻DevTinder</a>}
       </div>
-      <div className="flex gap-2 ">
+      <div className="flex gap-2 hidden  md:flex  ">
         {user && (
           <>
             <p className="mt-2  px-4 py-1  rounded-md transition-colors duration-200 hover:bg-gray-600 font-semibold cursor-pointer">
@@ -40,7 +41,7 @@ const NavBar = () => {
               <Link to="/connections">My Connections</Link>
             </p>
             <div className="mt-2 ml-2 indicator cursor-pointer">
-              <span className="indicator-item badge badge-secondary">12</span>
+              <span className="indicator-item badge badge-secondary size-6">{requests.length}</span>
               <Link
                 to="/requests"
                 className="px-4 py-1 rounded-md transition-colors duration-200 hover:bg-gray-600 font-semibold"
@@ -50,15 +51,48 @@ const NavBar = () => {
             </div>
           </>
         )}
-        {user && <p className="mt-3 ml-2 mx-1 ">Welcome, {user.firstName}</p>}
+        {/* {user && <p className="mt-3 ml-2 mx-1 ">Welcome, {user.firstName}</p>}
         {user && (
-          <div className="dropdown dropdown-end mx-5">
+          <div className="dropdown dropdown-end mx-5 ">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-10 rounded-full ">
+              <div className="w-10 rounded-full block">
+                <img alt="Tailwind CSS Navbar component" src={user?.photoUrl} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li className="text-lg">
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/connections">Connections</Link>
+              </li>
+              <li>
+                <Link to="/requests">Requests</Link>
+              </li>
+              <li>
+                <a onClick={handleLogoutClick}>Logout</a>
+              </li>
+            </ul>
+          </div>
+        )}{" "} */}
+      </div>
+          {/* Below this circle avatar will be visble in all screens */}
+          {user && <p className="mt-2 ml-3 mx-1 ">Welcome, {user.firstName}</p>}
+        {user && (
+          <div className="dropdown dropdown-end mx-5  ">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full block">
                 <img alt="Tailwind CSS Navbar component" src={user?.photoUrl} />
               </div>
             </div>
@@ -81,7 +115,7 @@ const NavBar = () => {
             </ul>
           </div>
         )}{" "}
-      </div>
+
     </div>
   );
 };
